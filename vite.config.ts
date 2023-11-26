f5fd73224da3d8945310c4a3e6f5aa41695ea9bf
Кирилл Капитальцев <jhonkoan110@gmail.com>
2023-11-26 16:31:47 +0300

    add auth pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
-import { defineConfig } from 'vite';
+import { Plugin, defineConfig } from 'vite';
+import { resolve } from 'path';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import handlebars from 'vite-plugin-handlebars';
 
 export default defineConfig({
-    plugins: [tsconfigPaths()],
+    plugins: [
+        tsconfigPaths(),
+        handlebars({
+            partialDirectory: resolve(__dirname, 'src/partials'),
+        }) as Plugin,
+    ],
 });
